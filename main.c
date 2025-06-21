666841ae14294128e88abde09f9f0f21c04ad6fe
isaacfz <isaacfz@chacfz.myguest.virtualbox.org>
2025-06-20 23:49:36 -0600

    Haciendo el llamado a las funciones de listas en el main

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 
 
 int main () {
-	printf("Hola mundo");
-	return 0;
+
+
+	struct NodeS* head = NULL;
+        struct NodeS* nodo1 = crearNodo(crearDatos(5).numeros);
+        insertarInicio(&head,nodo1->data.numeros); 
+
+        struct NodeS* nodo2 = crearNodo(crearDatos(10).numeros);
+        insertarFinal(&head,nodo2->data.numeros);
+   
+
+        struct NodeS* nodo3 = crearNodo(crearDatos(15).numeros);
+        insertarEnPos(&head,nodo3->data.numeros,2);
+
+        struct NodeS* nodo4 = crearNodo(crearDatos(10).numeros);
+        eliminarElemento(&head,nodo4->data.numeros);
+  
+
+    return 0;
+
+
+
+
+
+
 }
